7311c41634235cceb083ba0796a9308a6a15d112
CrawKatt <man849916@gmail.com>
2023-12-22 03:07:25 +0000

    22-12-2023-12:07-AM

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.2.0/unix_args.txt "$@"
+java -Xms4096M -Xmx30000M @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.2.0/unix_args.txt "$@"
